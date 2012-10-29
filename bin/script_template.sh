97756615a5edad71b0ef24c26742a2f9bb41ecd6
joshuacelliott <joshuacelliott@gmail.com>
2012-10-28 23:44:51 -0600

    added script to toggle home directory version control

--- bin/script_template.sh
+++ bin/script_template.sh
@@ -2,7 +2,8 @@
 
 # colorized output
 function cinfo() {
-  echo -e "\E[34m$1\E[0m"
+  # echo -e "\E[34m$1\E[0m" # blue
+  echo -e "\E[32m$1\E[0m" # green
 }
 function cwarn() {
   echo -e "\E[33m$1\E[0m"
