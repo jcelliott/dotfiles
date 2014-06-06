02cc7b2f9be4ec562afee93397587908a99fb648
Joshua Elliott <joshuacelliott@gmail.com>
2014-06-05 18:08:08 -0600

    Preliminary custom bindings for fish vi mode

---
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,20 @@
+function fish_user_key_bindings --description "User key bindings for fish"
+  fish_vi_key_bindings
+
+  # normal mode
+  bind -m insert \n execute
+  bind \cl clear 'commandline ""' execute
+  bind -e -k home
+  bind -k home beginning-of-line
+  bind H beginning-of-line
+  bind L end-of-line
+
+  # insert mode
+  bind -M insert \cl clear 'commandline ""' execute
+  bind -e -M insert -k home
+  bind -M insert -k home beginning-of-line
+  bind -M insert \cw beginning-of-line
+  bind -M insert \ce end-of-line
+
+end
+
