e60a0b550506a9c1525ca8bc29daae53262252b7
Joshua Elliott <joshuacelliott@gmail.com>
2015-03-24 11:27:04 -0600

    [fish] update some key bindings

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,10 +1,9 @@
 function fish_user_key_bindings --description "User key bindings for fish"
   fish_vi_key_bindings
 
-  # fzf (fuzzy finder [github.com/junegunn/fzf]) 
-  if test -e $HOME/.local/share/fzf
-    fzf_key_bindings
-  end
+  # reverse search (https://github.com/jbonjean/re-search)
+  bind \cr re_search
+  bind -M insert \cr re_search
 
   # normal mode
   bind -m insert \n execute
@@ -14,6 +13,7 @@ function fish_user_key_bindings --description "User key bindings for fish"
   bind H beginning-of-line
   bind L end-of-line
   bind \es prepend_sudo
+  bind $argv -k f1 __fish_man_page
 
   # insert mode
   bind -M insert \cl clear 'commandline ""' execute
@@ -24,6 +24,13 @@ function fish_user_key_bindings --description "User key bindings for fish"
   bind -M insert \cf accept-autosuggestion
   bind -M insert \cg suppress-autosuggestion
   bind -M insert \es prepend_sudo
+  bind -M insert $argv -k f1 __fish_man_page
+  bind -M insert \ej backward-word
+  bind -M insert \ek forward-word
+  bind -M insert \eh backward-char
+  bind -M insert \el forward-char
+  bind -M insert \eH beginning-of-line
+  bind -M insert \eL end-of-line
 
 end
 
