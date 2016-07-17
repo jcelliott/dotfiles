97676b8adf81d9af5f3b29b606345a3989b3605a
Joshua Elliott <joshuacelliott@gmail.com>
2016-07-17 12:04:03 -0600

    [fish] improvements to fish_prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -13,30 +13,50 @@ function fish_prompt --description 'Write out the prompt'
         set -g __fish_prompt_cwd (set_color $fish_color_cwd)
     end
 	if not set -q __fish_prompt_user
-		set -g __fish_prompt_user (set_color $fish_color_user)
+        if set -q fish_prompt_show_user
+            set -g __fish_prompt_user (set_color $fish_color_user)"$USER"(set_color normal)
+        else
+            set -g __fish_prompt_user ""
+        end
 	end
 	if not set -q __fish_prompt_host
-		set -g __fish_prompt_host (set_color $fish_color_host)
+        if set -q fish_prompt_show_host
+            set -g __fish_prompt_host "@"(set_color $fish_color_host)"$__fish_prompt_hostname"(set_color normal)
+        else
+            set -g __fish_prompt_host ""
+        end
 	end
 
 	if not set -q -g __fish_update_functions_defined
 		set -g __fish_update_functions_defined
 
-		function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
+		function __fish_repaint_user -v fish_color_user -d "repaint when fish_color_user changes"
+			if status --is-interactive
+				set -e __fish_prompt_user
+				commandline -f repaint ^/dev/null
+			end
+		end
+		function __fish_repaint_user -v fish_prompt_show_user -d "repaint when fish_prompt_show_user changes"
 			if status --is-interactive
 				set -e __fish_prompt_user
 				commandline -f repaint ^/dev/null
 			end
 		end
 
-		function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
+		function __fish_repaint_host -v fish_color_host -d "repaint when fish_color_host changes"
+			if status --is-interactive
+				set -e __fish_prompt_host
+				commandline -f repaint ^/dev/null
+			end
+		end
+		function __fish_repaint_host -v fish_prompt_show_host -d "repaint when fish_prompt_show_host changes"
 			if status --is-interactive
 				set -e __fish_prompt_host
 				commandline -f repaint ^/dev/null
 			end
 		end
 
-		function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
+		function __fish_repaint_status --on-variable fish_color_status -d "Event handler; repaint when fish_color_status changes"
 			if status --is-interactive
 				set -e __fish_prompt_status
 				commandline -f repaint ^/dev/null
@@ -51,6 +71,9 @@ function fish_prompt --description 'Write out the prompt'
 
   # echo "--- $history[1]"
   # echo "--- $history[2]"
+  # echo "--- last_status $last_status"
+  # echo "--- __fish_last_status $__fish_last_status"
+  #
 	# set -l prompt_status
   # # if test $last_status -ne $__fish_last_status -o $history[1] -ne $history[2]
   # if test $last_status -ne $__fish_last_status
@@ -64,6 +87,8 @@ function fish_prompt --description 'Write out the prompt'
   #   end
   #   set -g __fish_last_status $last_status
   # end
+  #
+    set -g __fish_last_status $last_status
 
 	set -l prompt_status
 	if test $last_status -ne 0
@@ -81,7 +106,7 @@ function fish_prompt --description 'Write out the prompt'
   end
 
     # full prompt
-	echo -ns "$__fish_prompt_user" "$USER" "$__fish_prompt_normal" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" (fish_vi_prompt_mode) ' '
+	echo -ns "$__fish_prompt_user" "$__fish_prompt_host" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" (fish_vi_prompt_mode) ' '
     # short prompt
 	# echo -ns ' ' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" "$prompt_status" (fish_vi_prompt_mode) ' '
 end
