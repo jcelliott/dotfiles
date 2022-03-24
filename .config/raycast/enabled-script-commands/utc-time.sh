c696a0fcad97750b2756a9a544141c0570259f68
Joshua Elliott <joshuacelliott@gmail.com>
2022-03-24 11:00:49 -0600

    [raycast] add UTC time script command

---
+++ .config/raycast/enabled-script-commands/utc-time.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# @raycast.title UTC Time
+# @raycast.schemaVersion 1
+# @raycast.mode inline
+# @raycast.refreshTime 10s
+# @raycast.packageName Developer Utilities
+# @raycast.icon 🕐
+# @raycast.description Show UTC time
+
+echo -e "\x1b[32m$(date -u)\x1b[0m"
